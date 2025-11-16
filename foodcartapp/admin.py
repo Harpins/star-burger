2c6495719619d16db893d89534767e7bb5c5455e
Igor Derevnin <shroom2207@yandex.ru>
2025-11-16 22:31:37 +0300

    added return to 'manager/orders' after edit in admin

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,74 +1,85 @@
 from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
+from django.http import HttpResponseRedirect
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 from django.db.models import Sum, F
 
-from .models import Product, ProductCategory, Restaurant, RestaurantMenuItem, Order, OrderItem
+from .models import (
+    Product,
+    ProductCategory,
+    Restaurant,
+    RestaurantMenuItem,
+    Order,
+    OrderItem,
+)
 
-class OrderItemInline(admin.TabularInline): 
+
+class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    extra = 0  
-    min_num = 1  
-    readonly_fields = ['price_at_order'] 
-    
-    fields = ['product', 'quantity', 'price_at_order']
+    extra = 0
+    min_num = 1
+    readonly_fields = ["price_at_order"]
 
-    
+    fields = ["product", "quantity", "price_at_order"]
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
-        'id', 
-        'firstname', 
-        'lastname', 
-        'phonenumber', 
-        'address',
-        'created_at',
-        'get_total_order_price',
-        'get_items_count'
+        "id",
+        "firstname",
+        "lastname",
+        "phonenumber",
+        "address",
+        "created_at",
+        "get_total_order_price",
+        "get_items_count",
     ]
-    
-    list_filter = ['created_at']
-    search_fields = ['firstname', 'lastname', 'phonenumber', 'address']
-    readonly_fields = ['created_at', 'updated_at', 'get_total_order_price']
-    
+
+    list_filter = ["created_at"]
+    search_fields = ["firstname", "lastname", "phonenumber", "address"]
+    readonly_fields = ["created_at", "updated_at", "get_total_order_price"]
+
     inlines = [OrderItemInline]
-    
+
     fieldsets = [
-        ('Информация о клиенте', {
-            'fields': [
-                'firstname', 
-                'lastname', 
-                'phonenumber', 
-                'address'
-            ]
-        }),
-        ('Даты', {
-            'fields': [
-                'created_at', 
-                'updated_at'
-            ],
-            'classes': ['collapse']
-        }),
-        ('Итоговая стоимость', {
-            'fields': ['get_total_order_price']
-        }),
+        (
+            "Информация о клиенте",
+            {"fields": ["firstname", "lastname", "phonenumber", "address"]},
+        ),
+        ("Даты", {"fields": ["created_at", "updated_at"], "classes": ["collapse"]}),
+        ("Итоговая стоимость", {"fields": ["get_total_order_price"]}),
     ]
-    
+
     def get_total_order_price(self, obj):
-        total = obj.items.aggregate(total=Sum(F('quantity') * F('price_at_order')))['total']
+        total = obj.items.aggregate(total=Sum(F("quantity") * F("price_at_order")))[
+            "total"
+        ]
         return f"{total} руб." if total else "0 руб."
-    get_total_order_price.short_description = 'Итоговая стоимость'
-    
-    
+
+    get_total_order_price.short_description = "Итоговая стоимость"
+
     def get_items_count(self, obj):
-        """Количество позиций в заказе"""
         return obj.items.count()
-    get_items_count.short_description = 'Кол-во позиций'
-    
+
+    get_items_count.short_description = "Кол-во позиций"
+
     def get_queryset(self, request):
-        """Оптимизация запроса с prefetch_related"""
-        return super().get_queryset(request).prefetch_related('items', 'items__product')
+        return super().get_queryset(request).prefetch_related("items", "items__product")
+
+    def response_post_save_change(self, request, obj):
+        response = super().response_post_save_change(request, obj)
+        if "next" in request.GET:
+            next_url = request.GET["next"]
+            allowed_host = request.get_host()
+            if url_has_allowed_host_and_scheme(
+                next_url, allowed_hosts={allowed_host}, require_https=False
+            ):
+                return HttpResponseRedirect(next_url)
+        return response
+
 
 class RestaurantMenuItemInline(admin.TabularInline):
     model = RestaurantMenuItem
@@ -78,93 +89,87 @@ class RestaurantMenuItemInline(admin.TabularInline):
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     search_fields = [
-        'name',
-        'address',
-        'contact_phone',
+        "name",
+        "address",
+        "contact_phone",
     ]
     list_display = [
-        'name',
-        'address',
-        'contact_phone',
+        "name",
+        "address",
+        "contact_phone",
     ]
-    inlines = [
-        RestaurantMenuItemInline
-    ]
+    inlines = [RestaurantMenuItemInline]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = [
-        'get_image_list_preview',
-        'name',
-        'category',
-        'price',
-        'id'
-    ]
+    list_display = ["get_image_list_preview", "name", "category", "price", "id"]
     list_display_links = [
-        'name',
+        "name",
     ]
     list_filter = [
-        'category',
+        "category",
     ]
     search_fields = [
         # FIXME SQLite can not convert letter case for cyrillic words properly, so search will be buggy.
         # Migration to PostgreSQL is necessary
-        'name',
-        'category__name',
+        "name",
+        "category__name",
     ]
 
-    inlines = [
-        RestaurantMenuItemInline
-    ]
+    inlines = [RestaurantMenuItemInline]
     fieldsets = (
-        ('Общее', {
-            'fields': [
-                'name',
-                'category',
-                'image',
-                'get_image_preview',
-                'price',
-            ]
-        }),
-        ('Подробно', {
-            'fields': [
-                'special_status',
-                'description',
-            ],
-            'classes': [
-                'wide'
-            ],
-        }),
+        (
+            "Общее",
+            {
+                "fields": [
+                    "name",
+                    "category",
+                    "image",
+                    "get_image_preview",
+                    "price",
+                ]
+            },
+        ),
+        (
+            "Подробно",
+            {
+                "fields": [
+                    "special_status",
+                    "description",
+                ],
+                "classes": ["wide"],
+            },
+        ),
     )
 
-    readonly_fields = [
-        'get_image_preview',
-        'id'
-    ]
+    readonly_fields = ["get_image_preview", "id"]
 
     class Media:
-        css = {
-            "all": (
-                static("admin/foodcartapp.css")
-            )
-        }
+        css = {"all": (static("admin/foodcartapp.css"))}
 
     def get_image_preview(self, obj):
         if not obj.image:
-            return 'выберите картинку'
-        return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
-    get_image_preview.short_description = 'превью'
+            return "выберите картинку"
+        return format_html(
+            '<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url
+        )
+
+    get_image_preview.short_description = "превью"
 
     def get_image_list_preview(self, obj):
         if not obj.image or not obj.id:
-            return 'нет картинки'
-        edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
-        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
-    get_image_list_preview.short_description = 'превью'
+            return "нет картинки"
+        edit_url = reverse("admin:foodcartapp_product_change", args=(obj.id,))
+        return format_html(
+            '<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>',
+            edit_url=edit_url,
+            src=obj.image.url,
+        )
+
+    get_image_list_preview.short_description = "превью"
 
 
 @admin.register(ProductCategory)
 class ProductAdmin(admin.ModelAdmin):
     pass
-
